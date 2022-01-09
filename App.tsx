ec162b9d7232cc24f9b9c01030e79006752c4ab4
felipeleite11 <lipi4shared@hotmail.com>
2022-01-09 11:44:13 -0300

    Sorting the app list.

--- App.tsx
+++ App.tsx
@@ -29,6 +29,16 @@ export default function App() {
 
     const currentPasswords = (JSON.parse(currentPasswordsString) as StoredPasswords)
 
+    currentPasswords.sort((a, b) => {
+      if(a.title < b.title) {
+        return -1
+      }
+      if(a.title > b.title) {
+        return 1
+      }
+      return 0
+    })
+
     setPasswordList(currentPasswords)
   }
 
--- SUGESTÕES.md
+++
@@ -1,7 +0,0 @@
-Permitir cadastrar link da página de login
-
-Permitir apagar todas as senhas salvas
-
-Permtir informar (checkbox) se  o login tem 2FA
-
-Ordenar a lista
